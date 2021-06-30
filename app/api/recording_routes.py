30e80f2a2c212f39e347a12a63da9c54a554a956
Melonnie <melonnie13@gmail.com>
2021-06-30 17:26:24 -0500

    started backend routes

--- app/api/recording_routes.py
+++ app/api/recording_routes.py
@@ -3,3 +3,18 @@ from app.models import Recording, db
 from flask_login import current_user, login_required
 
 recording_routes = Blueprint('recordings', __name__)
+
+
+@recording_routes.route('', methods=['POST'])
+@login_required
+def upload_recording():
+    new_recording = Recording(
+        title=request.form['title'],
+        description=request.form['description'],
+        audio=request.form['audio'],
+        user_id=current_user.id,
+        category_id=request.form['category_id']
+    )
+    db.session.add(new_recording)
+    db.session.commit()
+    return new_recording.to_dict()
---
+++ react-app/src/store/recording.js
@@ -0,0 +1,17 @@
+//constants
+
+const ADD_RECORDING = 'recording/ADD_RECORDING';
+
+
+//action creators
+
+const addRecording = (recording) => ({
+    type: ADD_RECORDING,
+    recording
+})
+
+//thunks
+
+//initial state
+
+//reducer
