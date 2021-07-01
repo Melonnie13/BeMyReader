6f9872355509b553ac914412c56643c9489e498f
Melonnie <melonnie13@gmail.com>
2021-06-30 20:06:39 -0500

    working on audio recorder

--- react-app/src/components/UploadRecording/index.js
+++ react-app/src/components/UploadRecording/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { ReactMic } from 'react-mic';
+import { ReactAudioPlayer } from 'react-audio-player';
 import { uploadRecording } from '../../store/recording';
 import './UploadRecording.css'
 
@@ -10,6 +11,36 @@ const UploadRecording = () => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user);
 
-    const
+    const [record, setRecord] = useState(false);
+    const [recordingBlob, setRecordingBlob] = useState('');
+    const [audio, setAudio] = useState(false);
+    const [formOpen, setFormOpen] = useState(false);
+
+    const startRecording = () => {
+        setRecord(true);
+    }
+
+    const stopRecording = () => {
+        setRecord(false);
+    }
+
+    const onStop = blob => {
+        setRecordingBlob(blob)
+        setAudio(true);
+    }
+    const onDelete = () => {
+        setRecordingBlob('');
+        setAudio(false);
+    }
+    const onUpload = () => {
+        setFormOpen(!formOpen);
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append();
+    }
+
+
 
 }
