8969b93b0668424b6324b8a434753666bf1b4764
Melonnie <melonnie13@gmail.com>
2021-07-03 20:54:01 -0500

    working on comment routes

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -11,5 +11,38 @@ def upload_comment():
     new_comment = Comment(
         body=request.form['body'],
         user_id=current_user.id,
-        recording_id=[]
+        recording_id=['recording']
     )
+    db.session.add(new_comment)
+    db.session.commit()
+    return new_comment.to_dict()
+
+# @comment_routes.route('/<int:recording_id>')
+# def get_recording_comments(recording_id):
+#     # print('comments from commentroutes ======================================')
+#     comments = Comment.query.filter(Comment.recording_id == recording_id).all()
+#     return {'comments': [comment.to_dict() for comment in comments]}
+
+
+# @comment_routes.route("/delete/<int:id>", methods=["DELETE"])
+# @login_required
+# def delete_comment(id):
+#     comment = Comment.query.get(id)
+#     if not comment:
+#         return jsonify('comment not found')
+#     db.session.delete(comment)
+#     db.session.commit()
+#     return {'id': id}
+
+
+# @comment_routes.route("/update/<int:id>", methods=["PUT"])
+# # @login_required
+# def update_comment(id):
+#     print("made it here =====", request.form["comment"])
+#     edit_comment = Comment.query.get(id)
+#     edit_comment.comment = request.form["comment"]
+#    # if not edit_comment:
+#    #     return jsonify('comment not found')
+#     db.session.add(edit_comment)
+#     db.session.commit()
+#     return edit_comment.to_dict()
---
+++ react-app/src/store/comment.js
@@ -0,0 +1,105 @@
+//constants
+const SET_COMMENTS = "comment/SET_COMMENTS";
+const ADD_COMMENT = "comment/ADD_COMMENT";
+const REMOVE_COMMENT = "comment/REMOVE_COMMENT";
+const EDIT_COMMENT = 'comment/EDIT_COMMENT';
+
+//action creators
+const setComments = (comment) => ({
+    type: SET_COMMENTS,
+    payload: comment,
+});
+
+const postComment = (comment) => ({
+    type: ADD_COMMENT,
+    payload: comment
+});
+
+const deleteComment = (comment) => ({
+    type: REMOVE_COMMENT,
+    payload: comment
+});
+
+const editComment = (comment) => ({
+    type: EDIT_COMMENT,
+    payload: comment
+ });
+
+ //thunks
+export const renderPhotoComments = (recording_id) => async (dispatch) => {
+    const res = await fetch(`/api/comments/${recording_id}`);
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+      dispatch(setComments(data.comments));
+    }
+  };
+
+
+export const addComment = (formData) => async (dispatch) => {
+    const res = await fetch("/api/comments/new", {
+        method: "POST",
+        body: formData
+    });
+    if (res.ok) {
+        const addedComment = await res.json();
+        dispatch(postComment(addedComment))
+        return addedComment;
+      }
+};
+
+export const removeComment = (id) => async (dispatch) => {
+    const res = await fetch(`/api/comments/delete/${id}`, {
+        method:"DELETE",
+        body: JSON.stringify(id)
+    })
+    // console.log('id from comment.js=======', {id})
+    if(res.ok) {
+        const deletedComment = await res.json()
+        dispatch(deleteComment(deletedComment))
+        return deletedComment;
+    }
+};
+
+export const updateComment = (formData, comment_id) => async (dispatch) => {
+    const res = await fetch(`/api/comments/update/${comment_id}`, {
+        method:"PUT",
+        body: formData
+    })
+    if(res.ok) {
+        const editedComment = await res.json()
+        dispatch(editComment(editedComment))
+        return editedComment;
+    }
+};
+
+// reducer
+
+const initialState = {};
+
+export default function reducer(state = initialState, action) {
+    const newState = {...state};
+    switch (action.type) {
+        case SET_COMMENTS:
+            action.payload.forEach((comment) =>{
+                newState[comment.id] = comment;
+            })
+            return newState;
+
+        case ADD_COMMENT:
+            // const singleState = {...state};
+            newState[action.payload.id] = action.payload;
+            return newState;
+
+        case REMOVE_COMMENT:
+            delete newState[action.payload.id]
+            return newState;
+
+        case EDIT_COMMENT:
+            newState[action.payload.id] = action.payload;
+            return newState;
+
+        default:
+            return state;
+    }
+}
