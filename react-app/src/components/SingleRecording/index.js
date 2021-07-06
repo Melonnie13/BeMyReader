263902bd427fe6f32f8591aa88225a9c38236a4b
Melonnie <melonnie13@gmail.com>
2021-07-06 11:27:46 -0500

    testing blob

--- react-app/src/components/SingleRecording/index.js
+++ react-app/src/components/SingleRecording/index.js
@@ -14,7 +14,7 @@ const SingleRecording = () => {
     const {id} = useParams();
     const recording = useSelector((state) => state.recording);
     const recordingUserId = recording.user_id
-    // console.log(recording.user_id, '****************** Recording userIDfrom SingleRecording Component*****')
+    // console.log(recording, '****************** Recording from SingleRecording Component*****')
     const user = useSelector(state => state.session.user)
     // console.log(user, "USER FROM SINGLE RECORDING COMPONENT********************")
 
