8fcdccbebb12eda2d5e84f4cd8cda8f9a07beadb
Melonnie <melonnie13@gmail.com>
2021-07-09 16:24:42 -0500

    splash page css

--- react-app/src/components/MainPage/index.js
+++ react-app/src/components/MainPage/index.js
@@ -2,37 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AboutMe from '../AboutMe'
 import './MainPage.css';
-import header from '../../Images/header.png';
+import banner from '../../Images/banner.png';
 
 const MainPage = () => {
     return (
         <div id='main-page'>
             <div id='main-page-welcome'>
                 <label htmlFor='welcome'></label>
-                <h2 id='main-page-welcome' name='welcome'>Welcome to</h2>
+                <h2 id='main-page-welcome-text' name='welcome'>Welcome to Be My Reader</h2>
             </div>
-            <div id='banner'>
-                <banner><img id='banner' src={header} alt='Be My Reader Header'/></banner>
-            </div>
+            {/* <div id='banner-container'>
+            <img id='banner-image' src={banner} alt='Be My Reader Banner'/>
+            </div> */}
             <div id='main-page-text'>
                 <label htmlFor='intro'></label>
-                <h3 id='main-page-text' name='intro'>The first ever website created specifically for the Blind and Visually Impaired.</h3>
+                <h3 id='main-page-text' name='intro'>the first ever website created specifically for the Blind and Visually Impaired</h3>
             </div>
-            <div id='main-page-listen'>
-                <Link to='/recordings/alphabetical'>
-                    <label htmlFor='listen-button'></label>
-                    <button name='listen-button' className='big-button'>
-                        LISTEN
-                    </button>
-                </Link>
-            </div>
-            <div id='main-page-record'>
-                <Link to='/recording/upload'>
-                    <label htmlFor='record-button'></label>
-                    <button name='record-button' className='big-button'>
-                        RECORD
-                    </button>
-                </Link>
+            <div id='main-page-button-container'>
+                <div id='main-page-listen'>
+                    <Link to='/recordings/alphabetical'>
+                        <label htmlFor='listen-button'></label>
+                        <button name='listen-button' className='big-button'>
+                            LISTEN
+                        </button>
+                    </Link>
+                </div>
+                <div id='main-page-record'>
+                    <Link to='/recording/upload'>
+                        <label htmlFor='record-button'></label>
+                        <button name='record-button' className='big-button'>
+                            RECORD
+                        </button>
+                    </Link>
+                </div>
             </div>
             <div id='about-me'>
                 <AboutMe />
