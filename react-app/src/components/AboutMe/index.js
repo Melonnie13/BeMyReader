8adac3f756210ef07516465a8b04fc3168d40b59
Melonnie <melonnie13@gmail.com>
2021-07-09 21:56:06 -0500

    css

--- react-app/src/components/AboutMe/index.js
+++ react-app/src/components/AboutMe/index.js
@@ -7,13 +7,15 @@ const AboutMe = () =>{
             <h2>Developed By: Melonnie Hicks</h2>
             <div id='about-me-logo'>
             <img id='my-avi' src="https://avatars.githubusercontent.com/u/76574880?v=4" alt="Melonnie" />
-            <div>
-                <label htmlFor='github-link'></label>
-                <a className='about-me-links' name='github-link' href="https://github.com/Melonnie13"><i className="fab fa-github"></i></a>
-            </div>
-            <div>
-                <label htmlFor='linkedIn-link'></label>
-                <a className='about-me-links' name='linkedIn-link' href="https://www.linkedin.com/in/melonniehicks/"> <i className="fab fa-linkedin"></i></a>
+            <div id='about-me-links-container'>
+                <div>
+                    <label htmlFor='github-link'></label>
+                    <a className='about-me-links' name='github-link' href="https://github.com/Melonnie13"><i className="fab fa-github fa-2x"></i></a>
+                </div>
+                <div>
+                    <label htmlFor='linkedIn-link'></label>
+                    <a className='about-me-links' name='linkedIn-link' href="https://www.linkedin.com/in/melonniehicks/"> <i className="fab fa-linkedin fa-2x"></i></a>
+                </div>
             </div>
             </div>
         </div>
